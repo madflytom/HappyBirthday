424eb6da2b63865d4259500c6c05109e8962cbb7
Tom Stahl <madflytom@gmail.com>
2022-12-04 07:34:15 -0500

    Initial commit

---
+++ app/src/test/java/com/example/happybirthday/ExampleUnitTest.kt
@@ -0,0 +1,26 @@
+package com.example.happybirthday
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun stringCompare_isCorrect() {
+        val stringReturn = getString()
+        assertEquals("Hey there", stringReturn)
+    }
+
+    fun getString(): String {
+        return "Hey there"
+    }
+}
